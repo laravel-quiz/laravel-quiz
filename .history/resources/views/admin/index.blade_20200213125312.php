70d6dbe8b68a1831b0cf8b73dbcd98b66d86a128
Anil Stha <anilshr25@gmail.com>
2020-02-13 13:11:06 +0545

    Added Admin Template

---
+++ .history/resources/views/admin/index.blade_20200213125312.php
@@ -0,0 +1,27 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <!--**********************************
+        Content body start
+    ***********************************-->
+    <div class="content-body">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="user-img c-pointer position-relative" data-toggle="dropdown">
+                                    <h1>Hello</h1>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <!-- #/ container -->
+    </div>
+    <!--**********************************
+        Content body end
+    ***********************************-->
+@endsection
