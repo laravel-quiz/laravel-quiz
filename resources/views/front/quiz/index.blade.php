2251d965814a3d9796a8f0aa0a4c2c8799fdb69a
hamlet <imhamlet.hams@gmail.com>
2020-02-13 15:51:19 +0545

    options in quiz

--- resources/views/front/quiz/index.blade.php
+++ resources/views/front/quiz/index.blade.php
@@ -7,6 +7,9 @@
                     Quiz
                 </div>
                 <div id="quiz">
+                    <h1 id="question"></h1>
+                    <div id="answers">
+                    </div>
                 </div>
             </div>
 @endsection
@@ -16,6 +19,7 @@
 <script src="{{ asset('/js/jquery-3.4.1.min.js') }}"></script>
 <script type="text/javascript">
 $(document).ready(function(){
+    var index = 0;
      $.ajaxSetup({
       headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -25,12 +29,22 @@ $(document).ready(function(){
      $.get('https://cors-anywhere.herokuapp.com/https://opentdb.com/api.php?amount=10',function(data){
         console.log(data);
         var results = data.results;
-        console.log(results);
+
         var output = '';
+        var questions = new Array();
         results.forEach(function(quiz){
-            output += `<h3> ${quiz.question} </h3>`;
+            questions.push(quiz);
+            //output += `<h3> ${quiz.question} </h3>`;
         });
-        $('#quiz').append(output);
+        console.log(questions);
+        var value = questions[index].incorrect_answers;
+        $('#question').text(questions[index].question);
+        output += `<br><input type="radio" name="answers" value="${questions[index].correct_answer}"><label>${questions[index].correct_answer}</label><br>`
+        value.forEach(function(d){
+            output += `<input type="radio" name="answers" value="${d}"><label>${d}</label><br>`
+        });
+        $('#question').append(output);
+        // $('#quiz').append(output);
      });
 
     // var url = 'https://cors-anywhere.herokuapp.com/https://opentdb.com/api.php?amount=10';
