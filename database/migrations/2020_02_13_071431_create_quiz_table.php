54a0029cde2eb3c79170212c5d674cef45e6a7bd
hamlet <imhamlet.hams@gmail.com>
2020-02-13 13:09:12 +0545

    database setup for quiz

---
+++ database/migrations/2020_02_13_071431_create_quiz_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quiz', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('category');
+            $table->string('type');
+            $table->string('difficulty');
+            $table->string('question');
+            $table->string('correct_answer');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quiz');
+    }
+}
