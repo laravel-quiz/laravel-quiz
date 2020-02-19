5a5920a5b02fe40f350ae2623fb5a89fa0b867ae
Anil Stha <anilshr25@gmail.com>
2020-02-19 13:46:09 +0545

    users

--- .history/routes/web_20200219132049.php
+++ .history/routes/web_20200219132049.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
+
+
 Route::middleware(['auth','role'])->prefix('/admin')->group(function(){
     Route::get('/dashboard','AdminController@index')->name('admin.index');
 
