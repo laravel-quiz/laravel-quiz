3960c338269885cf7ee3c053fad852bd4cbdfb52
sandesh shrestha <sandeshshrestha63@gmail.com>
2020-02-23 14:20:34 +0545

    backend change and design

--- .history/app/services/UserServices_20200221091519.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Policies;
-
-use App\User;
-public class UserServices
-{
-    public function __construct(User $user)
-    {}
-}
--- .history/app/services/UserServices_20200221091543.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Policies;
-
-use App\User;
-class UserServices
-{
-    public function __construct(User $user)
-    {}
-}
